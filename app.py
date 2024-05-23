2b7661652d45735e69cfc4bd95215de96976c21f
sky4704 <130532418+sky4704@users.noreply.github.com>
2024-05-23 13:26:15 +0000

    Dynamic data insertion

--- app.py
+++ app.py
@@ -2,10 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+}, {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 12,00,000'
+}, {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote'
+}, {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Fransico, USA',
+    'salary': '$120,000'
+}]
+
 
 @app.route("/")
 def hello_sky():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS, company_name='Sky')
 
 
 if __name__ == "__main__":
